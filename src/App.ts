825a9eb07759b1e8bd2312fe352530567d8b98e1
MzDobis <decagon@MacBook-Air-2.local>
2023-10-03 00:58:44 +0100

    Folders layout

--- src/App.ts
+++ src/App.ts
@@ -1,2 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv'
+
+const app = express()
+dotenv.config()
+const port = process.env.PORT
+
+
+
+app.listen(port, ()=>{
+    console.log(`app is listening on port ${port}`)
+})
